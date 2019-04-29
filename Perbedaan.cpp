83252559b3bb051b83a6294d4a24b3c7e6783c61
zhahrah <49123237+zhahrah@users.noreply.github.com>
2019-04-29 15:47:48 +0800

    Create Perbedaan.cpp

---
+++ Perbedaan.cpp
@@ -0,0 +1,7 @@
+Perbedaan linier dan sirkular:
+
+    Sirkular memiliki pointer next yang akan menunujuk diri sendiri sehingga berputar, sednagkan linier tidak berputar
+
+Perbedaan double dan single:
+
+    single memiliki satu variabel pointer saja, sedangkan double memiliki dua variabel pointer yang mennunujuk ke node selanjutnya dan node sebelumnya.
